a9f86cf19c53d9eee1df4b99f02df795000cec96
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-09-02 08:43:08 -0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        drawer: Text(
-          'data',
-          style: TextStyle(
-            color: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('MyApp'),
+        ),
+        drawer: Drawer(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 300,
+                    color: Colors.yellow,
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
